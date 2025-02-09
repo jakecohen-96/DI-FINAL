2966bae8594a8d93f9aebdb31c8e5dfe567e0a8a
jakecohen-96 <yakop.96@gmail.com>
2025-02-09 19:43:39 +0200

    test 2

--- chat-app/frontend/vite.config.ts
+++ chat-app/frontend/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
   server: {
-    host: "0.0.0.0",
-    port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
+    host: true,
+    port: 5173, 
+  },
+  preview: {
+    allowedHosts: ["chat-app-frontend-p209.onrender.com"],
   },
 });
