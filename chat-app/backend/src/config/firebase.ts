8f96995b75ff3a8841182c73e57362c1ac88ed60
jakecohen-96 <yakop.96@gmail.com>
2025-02-10 21:01:53 +0200

    testing backend deployment 1

--- chat-app/backend/src/config/firebase.ts
+++ chat-app/backend/src/config/firebase.ts
@@ -1,15 +1,33 @@
 import { initializeApp, cert } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
+import { getAnalytics } from "firebase/analytics";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_SDK as string);
+const firebaseConfig = {
 
-const app = initializeApp({
-  credential: cert(serviceAccount),
-});
+  apiKey: "AIzaSyDj6dCxv5i2GrnWCTINFJjpQI3IJudyg80",
 
+  authDomain: "jake-s-chat-app.firebaseapp.com",
+
+  databaseURL: "https://jake-s-chat-app-default-rtdb.europe-west1.firebasedatabase.app",
+
+  projectId: "jake-s-chat-app",
+
+  storageBucket: "jake-s-chat-app.firebasestorage.app",
+
+  messagingSenderId: "116931307401",
+
+  appId: "1:116931307401:web:fc32721c3f806e88afe358",
+
+  measurementId: "G-C3QFKKRFW8"
+
+};
+
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics();
 export const auth = getAuth(app);
 export const db = getFirestore(app);  
