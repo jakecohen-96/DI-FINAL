7d43b9caacaaeb08695df04b772f668d0f857c77
jakecohen-96 <yakop.96@gmail.com>
2025-02-12 21:48:57 +0200

    deployed on firebase

--- chat-app/frontend/vite.config.ts
+++ chat-app/frontend/vite.config.ts
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
   base: './',
-  server: {
-    host: true,
-    port: 5173,
-  },
-  preview: {
-    allowedHosts: ['chat-app-frontend-p209.onrender.com'],
-  },
-});
+  plugins: [react()]
+})
